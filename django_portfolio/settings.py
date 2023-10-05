1990953cd96c997a6a9c78928a566b6e3ac4cb61
LuisCantos <134717134+LuisCantos@users.noreply.github.com>
2023-10-05 11:53:06 -0500

    sa

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -125,7 +125,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/public/'
 
-ALLOWED_HOSTS = ['porfolio-jango.onrender.com']
+ALLOWED_HOSTS = ['luiscantospe.onrender.com']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
